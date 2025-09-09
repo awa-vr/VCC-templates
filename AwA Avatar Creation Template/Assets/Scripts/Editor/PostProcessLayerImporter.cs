1d515ca3470c81886fb787f4811a07c18bbf4942
AwA <63929325+awa-vr@users.noreply.github.com>
2025-09-09 21:55:56 +0200

    Update: PostProcess layer importer

--- AwA Avatar Creation Template/Assets/Scripts/Editor/PostProcessLayerImporter.cs
+++ AwA Avatar Creation Template/Assets/Scripts/Editor/PostProcessLayerImporter.cs
@@ -1,30 +1,24 @@
 using UnityEditor;
 using UnityEngine;
 
-namespace AwAVR.Geo
-{
+namespace AwAVR {
     [InitializeOnLoad]
-    public static class PostProcessLayerImporter
-    {
+    public static class PostProcessLayerImporter {
         const string LAYER_NAME = "PostProcessing";
 
-        static PostProcessLayerImporter()
-        {
+        static PostProcessLayerImporter() {
             AddLayer(LAYER_NAME);
         }
 
-        static void AddLayer(string layerName)
-        {
-            var tagManager = new SerializedObject(
-                AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
-            var layersProp = tagManager.FindProperty("layers");
-            for (int i = 8; i < layersProp.arraySize; i++)
-            {
-                var sp = layersProp.GetArrayElementAtIndex(i);
-                if (sp.stringValue == layerName) return;
-                if (string.IsNullOrEmpty(sp.stringValue))
-                {
-                    sp.stringValue = layerName;
+        static void AddLayer(string layerName) {
+            var tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+            SerializedProperty layersProp = tagManager.FindProperty("layers");
+
+            for (int i = 8; i < layersProp.arraySize; i++) {
+                var currentLayer = layersProp.GetArrayElementAtIndex(i);
+                if (currentLayer.stringValue == layerName) return;
+                if (string.IsNullOrEmpty(currentLayer.stringValue)) {
+                    currentLayer.stringValue = layerName;
                     tagManager.ApplyModifiedProperties();
                     Debug.Log($"Added layer '{layerName}'");
                     return;
